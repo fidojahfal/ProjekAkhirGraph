28469ea4e8627412143c45dbf910e917a87f116e
Kyroline <rizky.dewantoro69@gmail.com>
2022-07-11 19:40:10 +0700

    Initial commit

---
+++ src/city/city.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package city;
+import germanyrailway.Graph;
+
+/**
+ *
+ * @author Admin
+ */
+public class city {
+    public static void main(String[] args) {
+        Graph g = new Graph();
+    }
+}
